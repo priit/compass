d55e1582e0397dbe25beef5acfd17e65cba42c71
Chris Eppstein <chris@eppsteins.net>
2009-02-22 09:57:05 -0800

    Started working on a sass documentation tool.

--- lib/sass_extensions.rb
+++ lib/sass_extensions.rb
@@ -1,13 +1,21 @@
 require 'sass'
 
-module Sass::Script::Functions
-  COMMA_SEPARATOR = /\s*,\s*/
-  def nest(*arguments)
-    nested = arguments.map{|a| a.value}.inject do |memo,arg|
-      ancestors = memo.split(COMMA_SEPARATOR)
-      descendants = arg.split(COMMA_SEPARATOR)
-      ancestors.map{|a| descendants.map{|d| "#{a} #{d}"}.join(", ")}.join(", ")
+module Sass
+  module Script::Functions
+    COMMA_SEPARATOR = /\s*,\s*/
+    def nest(*arguments)
+      nested = arguments.map{|a| a.value}.inject do |memo,arg|
+        ancestors = memo.split(COMMA_SEPARATOR)
+        descendants = arg.split(COMMA_SEPARATOR)
+        ancestors.map{|a| descendants.map{|d| "#{a} #{d}"}.join(", ")}.join(", ")
+      end
+      Sass::Script::String.new(nested)
     end
-    Sass::Script::String.new(nested)
+  end
+
+  module Tree
+    class MixinDefNode < Node
+      attr_accessor :name, :args
+    end
   end
 end
